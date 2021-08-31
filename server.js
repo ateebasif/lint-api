e8f32f6e0494dd1afcb7de954c5f45b6fcf62a92
ateebasif902 <ateeb.asif902@gmail.com>
2021-08-31 17:27:11 +0500

    linter api configured

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const port = 3001 || process.env.PORT;
+const Linter = require("eslint").Linter;
+const linter = new Linter();
+
+var cors = require("cors");
+const bodyParser = require("body-parser");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/linter", (req, res) => {
+  if (req.method === "POST") {
+    const { code, EsLintRules } = req.body;
+    const verifyAndFix = linter.verifyAndFix(code, {
+      parserOptions: {
+        ecmaVersion: 12,
+        sourceType: "script",
+        ecmaFeatures: {},
+      },
+      rules: JSON.parse(EsLintRules)[0],
+      env: {
+        node: true,
+        browser: true,
+        mocha: true,
+      },
+    });
+    res.send({ verifyAndFix });
+  } else {
+    res.send({});
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
